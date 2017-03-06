d50185f42fecb666b47089a75082c7dd96adb093
Alfaro Sierra <manuel.alfaro-sierra-external@capgemini.com>
2017-03-06 13:40:23 +0100

    Patching...

--- bin/lazygifter.js
+++ bin/lazygifter.js
@@ -6,10 +6,5 @@ const Lazygifter = require('../lib/lazygifterlib.js');
 const ParamsManager = require('../lib/utils/paramsManager.js');
 
 let params = new ParamsManager();
-let options = params.get();
-if (options.help) {
-    ParamsManager.help();
-} else {
-    let lazygifter = new Lazygifter(options);
-    lazygifter.run();
-}
+let lazygifter = new Lazygifter(params.get());
+lazygifter.run();
